565033483c929f1fea80b32d768948335f84ad54
Himanshu631 <himanshupauljsr@gmail.com>
2023-05-23 00:22:01 +0530

    first commit

---
+++ pw-user/main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+security = HTTPBasic()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
+    # to run the application ip:port
+    # uvicorn main:app --host 0.0.0.0 --port 8000 --reload
